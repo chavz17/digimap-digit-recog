5991d180a2f4c18a5e60bfd713149c9c76a85091
chavz17 <gabriel_chavez@dlsu.edu.ph>
2023-04-21 19:16:04 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import gunicorn
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev'
+app.config['UPLOAD_FOLDER'] = 'static/files'
+
+# Load the model
+model = tf.keras.models.load_model('digit_recognition_10.model')
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def home():
+    img = request.files['image']
+    image_path = './static/files/' + img.filename
+    img.save(image_path)
+
+    # Preprocessing image
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    img = cv2.resize(img, (28, 28))
+    _, img = cv2.threshold(img, 140, 255, cv2.THRESH_BINARY)
+    img = np.invert(np.array([img]))
+    img = img.astype('float32') / 255.0
+
+    # Get the prediction from the model
+    prediction = model.predict(img)
+    data = np.argmax(prediction)
+
+    return render_template('index.html', data=data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
